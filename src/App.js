3e8f7864771e46600ea01e61acc21f4f5eaf3556
Fagner Santos <fagner.0001@outlook.com>
2021-05-12 19:36:41 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Contato from './Components/Contato';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Produtos from './Components/Produtos';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="contato" element={<Contato />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
